f80285b42601b2affa2dbde5c9be7829530eaa74
perdy <perdyimam49@gmail.com>
2023-03-06 19:34:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new typed(".multiple-text", {
+    Strings: ["Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+//Ga kepake ngentot yang bikin di GitHub
